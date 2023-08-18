45ac377e7ba0af509d382726cede5519d50b6fd6
ah96 <amarhalilovic96@gmail.com>
2023-08-18 13:55:12 +0200

    .

--- nodes/hello_gui_node.cpp
+++ nodes/hello_gui_node.cpp
@@ -30,18 +30,18 @@
 int main(int argc, char *argv[])
 {
 
-  ros::init(argc, argv, "hello_gui_node",ros::init_options::AnonymousName);
+  ros::init(argc, argv, "textual_explanation",ros::init_options::AnonymousName);
   QApplication a(argc, argv);
 
   HelloGui w;
 
   // set the window title as the node name
-  w.setWindowTitle(QString::fromStdString(
-                       ros::this_node::getName()));
+  //w.setWindowTitle(QString::fromStdString(ros::this_node::getName()));
+  w.setWindowTitle(QString::fromStdString("Textual Explanations"));
 
   // load the icon from our qrc file and set it as the application icon
-  QIcon icon(":/icons/my_gui_icon.png");
-  w.setWindowIcon(icon);
+  //QIcon icon(":/icons/my_gui_icon.png");
+  //w.setWindowIcon(icon);
 
   w.show();
   return a.exec();
